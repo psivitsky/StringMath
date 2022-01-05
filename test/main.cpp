79dd4d87295beaed64533f3de6e2de045a97c6d9
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-01-05 13:37:51 +0300

    Test syntax updated

--- test/main.cpp
+++ test/main.cpp
@@ -3,7 +3,8 @@
  * \brief
  * The "StringMath" test console application
  * \details
- * Create a text file in the application folder with test expressions such as "1+1=2".
+ * Create a text file in the application folder with test expressions.
+ * The test syntax is as follows: |test_name,expression_str=calc_result|
  * Specify the expressions results with precision 'precision'.
  * After that run the application and see the test results in the terminal.
  */
@@ -18,6 +19,8 @@
 
 static const int    precision = 1;
 
+bool    test_str_parser(const QString &testStr, QString &testName, QString &testExp, double &testRes);
+
 const QString file_name = "test.txt";
 
 int main(int argc, char *argv[])
@@ -29,42 +32,38 @@ int main(int argc, char *argv[])
     QFile f(file_name);
     if(f.open(QFile::ReadOnly))
     {
+        int failure_cnt = 0;
+
+        double test_res = 0.;
+        QString test_name = "";
+        QString test_exp = "";
+
         StringMath calc(precision);
 
-        int failure_cnt = 0;
         QString line = "";
         QTextStream in(&f);
         while(in.readLineInto(&line))
         {
             if(line.isEmpty())
+                continue;
+
+            if(!test_str_parser(line, test_name, test_exp, test_res))
             {
-                out << "\n";
+                out << "The test string parsing failure! " << line << "\n";
                 continue;
             }
 
-            QString exp_str = line.mid(0, line.indexOf("="));        //The test expression.
-
-            double res_val = line.mid(line.indexOf("=") + 1, line.size() - line.indexOf("=")).toDouble();        //The test etalon result.
-            //Rounding of test etalon result to 'precision'...
-            double res_int_part = static_cast<double>(floor(res_val));
-            double res_nonint_part = res_val - floor(res_val);
-            double prec_dec = pow(10., precision);
-            res_nonint_part = static_cast<double>(qRound(res_nonint_part * prec_dec)) / prec_dec;
-            res_val = res_int_part + res_nonint_part;
-
             try
             {
-                if(res_val == calc.string_process(exp_str))
-                    out << "Success!\t" << line << "\n";
-                else
+                if(test_res != calc.string_process(test_exp))
                 {
                     ++failure_cnt;
-                    out << "Failure!\t" << line << "\n";
+                    out << "Failure!\t\t" << test_name << "\n";
                 }
             }
             catch(StringMathError &err)
             {
-                out << "Exception!\t" << err.msg << "\n";
+                out << "Exception!\t\t" << test_name << "\t\t" << err.msg << "\n";
             }
 
         }
@@ -72,7 +71,7 @@ int main(int argc, char *argv[])
         if(failure_cnt == 0)
             out << "There are no failures!\n";
         else
-            out << "There are " << failure_cnt << " failures!\n";
+            out << "\nThere are " << failure_cnt << " failures!\n";
 
         f.close();
     }
@@ -80,4 +79,27 @@ int main(int argc, char *argv[])
         out << "Can't open the test file!\n";
 
     return 0;
+}
+
+bool test_str_parser(const QString &testStr, QString &testName, QString &testExp, double &testRes)
+{
+    testName = "";
+    testExp = "";
+
+    QRegExp test_str_checker("^\\|\\w+\\,[^=]+\\=[^|]+\\|$");
+
+    if(!test_str_checker.exactMatch(testStr))
+        return false;
+
+    testName = testStr.mid(testStr.indexOf("|") + 1, testStr.indexOf("," - testStr.indexOf("|")) - 1);
+    testExp = testStr.mid(testStr.indexOf(",") + 1, testStr.indexOf("=") - testStr.indexOf(",") - 1);
+    testRes = testStr.mid(testStr.indexOf("=") + 1, testStr.lastIndexOf("|") - testStr.indexOf("=") - 1).toDouble();
+
+    double test_res_int_part = static_cast<double>(floor(testRes));
+    double test_res_nonint_part = testRes - floor(testRes);
+    double prec_dec = pow(10., precision);
+    test_res_nonint_part = static_cast<double>(qRound(test_res_nonint_part * prec_dec)) / prec_dec;
+    testRes = test_res_int_part + test_res_nonint_part;
+
+    return true;
 }
