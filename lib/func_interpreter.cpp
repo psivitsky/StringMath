e24a193be7ceaaa53d361517d0757dd48f7274be
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-02-12 15:39:07 +0300

    FuncInterpreter class defined

--- lib/func_interpreter.cpp
+++ lib/func_interpreter.cpp
@@ -20,49 +20,91 @@ FuncInterpreter::FuncInterpreter(const QVector<StringMathConstant> &constants, c
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 void FuncInterpreter::interpret(const QString &expressionStr, QString &interpretedExpStr) const
 {
+    QString::const_iterator begin = expressionStr.begin();
+
     QString name = "";
-    extract_name(expressionStr, name);
-    check_name(name);
+    if(expressionStr.contains(opening_bracket))
+        extract_name(begin, expressionStr.end(), name);
 
-    double argument = extract_argument(expressionStr);
+    double argument = extract_argument(begin, expressionStr.end());
 
-    foreach(StringMathFunction function, functions_)
-    {
-        if(function.name() == name)
-            ;       // Function calculation
-    }
+    calculate(name, argument, interpretedExpStr);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The function name extractor method.\n
+ * The algebraic function name extractor method.\n
  * The function name is located before the opening bracket.
- * \param[in] strExpression The expression string.
- * \param[out] funcName The funtion name.
+ * \param[in,out] begin The iterator pointing to the first character in the processing string.
+ * \param[in] end The iterator pointing to the character after the last character in the processing string.
+ * \param[out] funcName The function name (without spaces).
 */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void FuncInterpreter::extract_name(const QString &strExpression, QString &funcName) const
+void FuncInterpreter::extract_name(QString::const_iterator &begin, QString::const_iterator end, QString &funcName) const
 {
+    funcName = "";
 
+    while((*begin != opening_bracket) && (begin != end))
+    {
+        funcName += *begin;
+        ++begin;
+    }
+
+    if(begin == end)
+        throw StringMathError("FuncInterpreter: there is no opening bracket!");
+
+    ++begin;        //Skipping the opening bracket.
+
+    spaces_removing(funcName);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The function name checker method.
- * \param[in] funcName The funtion name.
+ * The algebraic function argument extractor method.\n
+ * If the expression does not contain a function, then the expression itself is an argument.
+ * If the expression contains a function, then the argument is between brackets.
+ * \param[in] begin The iterator pointing to the character in the processing string (after the opening bracket).
+ * \param[in] end The iterator pointing to the character after the last character in the processing string.
+ * \return The function argument value.
 */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void FuncInterpreter::check_name(const QString &funcName) const
+double FuncInterpreter::extract_argument(QString::const_iterator begin, QString::const_iterator end) const
 {
+    QString argument = "";
+    while((begin != end) || (*begin != closing_bracket))
+    {
+        argument += *begin;
+        ++begin;
+    }
 
+    operand_processing(argument);
+
+    return argument.toDouble();
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The function argument extractor method.\n
- * The function argument is located between brackets.
- * \param[in] strExpression The expression string.
- * \return The function argument.
+ * The algebraic function calculation method.\n
+ * If the function name is empty, then there is no function in the expression. The function calculation result will be the argument.
+ * \param[in] funcName The function name.
+ * \param[in] funcArgument The function argument value.
+ * \param[out] calcResultStr The algebraic function calculation result.
 */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-double FuncInterpreter::extract_argument(const QString &strExpression) const
+void FuncInterpreter::calculate(const QString &funcName, double funcArgument, QString &calcResultStr) const
 {
+    double calc_result = 0.;
+    if(funcName.isEmpty())
+        calc_result = funcArgument;
+    else
+    {
+        bool f_calculated = false;
+        foreach(StringMathFunction function, functions_)
+        {
+            if(function.name() == funcName)
+                calc_result = function.calculate(funcArgument);
+        }
 
+        if(!f_calculated)
+            throw StringMathError("FuncInterpreter: the function \"" + funcName + "\" doesn't exist!");
+    }
+
+    calcResultStr = QString::number(calc_result, 'f', precision);
 }
--- lib/func_interpreter.h
+++ lib/func_interpreter.h
@@ -19,7 +19,7 @@ class FuncInterpreter : public OpInterpreter
 public:
     FuncInterpreter(const QVector<StringMathConstant> &constants,
                     const QVector<StringMathFunction> &functions);
-
+    //Methods...
     void    interpret(const QString &expressionStr, QString &interpretedExpStr) const;
 private:
     FuncInterpreter(const FuncInterpreter &obj);
@@ -27,9 +27,9 @@ private:
     //Containers...
     QVector<StringMathFunction> functions_;
     //Methods...
-    void    extract_name(const QString &strExpression, QString &funcName) const;
-    void    check_name(const QString &funcName) const;
+    void    extract_name(QString::const_iterator &begin, QString::const_iterator end, QString &funcName) const;
+    double  extract_argument(QString::const_iterator begin, QString::const_iterator end) const;
 
-    double  extract_argument(const QString &strExpression) const;
+    void    calculate(const QString &funcName, double funcArgument, QString &calcResultStr) const;
 };
 #endif // FUNCINTERPRETER_H
