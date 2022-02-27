149d7ba555933c97867c42254aea25998b3e0735
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-02-27 17:08:05 +0300

    StringMath class refactoring

--- lib/string_math.cpp
+++ lib/string_math.cpp
@@ -1,36 +1,17 @@
 #include "string_math.h"
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-/*!
- * The constructor.
- * \param[in] precisionValue The precision of calculation result (an integer value in range from 'min_precision_value' to 'max_precision_value').
-*/
+//! \details Constructor.
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-StringMath::StringMath(int precisionValue)
+StringMath::StringMath()
 {
-    set_precision(precisionValue);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The calculation precision setter function.
- * \param[in] precisionValue The precision of calculation result (an integer value in range from 'min_precision_value' to 'max_precision_value').
-*/
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::set_precision(int precisionValue)
-{
-    if(precisionValue < min_precision_value)
-        throw StringMathError("StringMath: the precision of calculation result is too small!");
-    if(precisionValue > max_precision_value)
-        throw StringMathError("StringMath: the precision of calculation result is too big!");
-
-    precision_ = precisionValue;
-}
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-/*!
- * The calculation constant adder function.
+ * The function of adding a constant.
  * \param[in] newConstant The new constant.
-*/
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::add_constant(const StringMathConstant &newConstant)
+void StringMath::add_constant(const StringMathConstant& newConstant)
 {
     if(newConstant.name().isEmpty())
         throw StringMathError("StringMath: the constant name is empty!");
@@ -38,83 +19,160 @@ void StringMath::add_constant(const StringMathConstant &newConstant)
     foreach(StringMathConstant constant, constants_)
     {
         if(newConstant.name() == constant.name())
-            throw StringMathError("StringMath: such a constant already exists!");
+            throw StringMathError(
+                "StringMath: such a constant already exists!");
     }
 
     constants_.push_back(newConstant);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The calculation constant replacer function.
- * \param[in] existingConstantName The name of existing constant.
- * \param[in] newConstantValue The new value.
-*/
+ * Constant replacement function.
+ * \param[in] existingConstantName The name of an existing constant.
+ * \param[in] newConstantValue New constant value.
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::replace_constant(const QString &existingConstantName, double newConstantValue)
+void StringMath::replace_constant(const QString& existingConstantName,
+                                  double         newConstantValue)
 {
+    bool f_replaced = false;
     foreach(StringMathConstant constant, constants_)
     {
         if(constant.name() == existingConstantName)
         {
             constant.set_value(newConstantValue);
-            return;
+            f_replaced = true;
+            break;
         }
     }
 
-    throw StringMathError("StringMath: there is no such constant for replacement!");
+    if(!f_replaced)
+        throw StringMathError(
+            "StringMath: there is no such constant for replacement!");
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The calculation constant remover function.
- * \param[in] existingConstant The existing constant.
-*/
+ * Constant remover function.
+ * \param[in] existingConstant An existing constant.
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::remove_constant(const StringMathConstant &existingConstant)
+void StringMath::remove_constant(const StringMathConstant& existingConstant)
 {
     if(!constants_.contains(existingConstant))
-        throw StringMathError("StringMath: there is no such constant for removal!");
+        throw StringMathError(
+            "StringMath: there is no such constant for removal!");
 
     constants_.removeOne(existingConstant);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The algebraic function adder function.
+ * The function of getting constants.
+ * \return The container with constants ('StringMathConstant' objects).
+ */
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+const QVector<StringMathConstant>& StringMath::constants() const
+{
+    return constants_;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+/*!
+ * Method of adding a function.
  * \param[in] newFunction The new function.
-*/
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::add_function(const StringMathFunction &newFunction)
+void StringMath::add_function(const StringMathFunction& newFunction)
 {
+    if(newFunction.name().isEmpty())
+        throw StringMathError("StringMath: the function name is empty!");
 
+    foreach(StringMathFunction function, functions_)
+    {
+        if(newFunction.name() == function.name())
+            throw StringMathError(
+                "StringMath: such a function already exists!");
+    }
+
+    functions_.push_back(newFunction);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The algebraic function replacer function.
+ * Method of replacing the function.
  * \param[in] existingFunctionName The name of existing function.
-*/
+ * \param[in] newFunction The function wrapper.
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::replace_function(const QString &existingFunctionName, std::function<double (double)> newFunction)
+void StringMath::replace_function(const QString& existingFunctionName,
+                                  std::function<double(double)> newFunction)
 {
+    bool f_replaced = false;
+    foreach(StringMathFunction function, functions_)
+    {
+        if(function.name() == existingFunctionName)
+        {
+            function.set_function(newFunction);
+            f_replaced = true;
+            break;
+        }
+    }
 
+    if(!f_replaced)
+        throw StringMathError(
+            "StringMath: there is no such function for replacement!");
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The algebraic function remover function.
- * \param[in] newFunction The existing function.
-*/
+ * Function remover method.
+ * \param[in] newFunction An existing function.
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::remove_function(const StringMathFunction &newFunction)
+void StringMath::remove_function(const StringMathFunction& existingFunction)
 {
+    if(!functions_.contains(existingFunction))
+        throw StringMathError(
+            "StringMath: there is no such function for removal!");
 
+    functions_.removeOne(existingFunction);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
- * The function of calculating the expression.
- * \param[in] strExpression The expression of QString type.
- * \param[out] calcResult The calculation result.
-*/
+ * The method of getting functions.
+ * \return The container with functions ('StringMathFunction' objects).
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-void StringMath::calculate(const QString &strExpression, QString &calcResult) const
+const QVector<StringMathFunction>& StringMath::functions() const
 {
-    ExpressionAnalyzer obj(precision_, constants_, functions_);
+    return functions_;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+/*!
+ * Expression calculation function.
+ * \param[in] strExpression Expression string.
+ * \return The result of the calculation ('double' value).
+ */
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+double StringMath::calculate(const QString& strExpression) const
+{
+    QString            calcResult;
+    ExpressionAnalyzer obj(max_precision, constants_, functions_);
     obj.analyze(strExpression, calcResult);
+
+    return calcResult.toDouble();
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+/*!
+ * Expression calculation function.
+ * \param[in] strExpression Expression string.
+ * \param[in] precision Calculation precision (non-negative integer value
+ * between 'min_precision' and 'max_precision').
+ * \return The result of the calculation (string).
+ */
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+QString& StringMath::calculate(const QString& strExpression,
+                               int            precision) const
+{
+    static QString     calcResult;
+    ExpressionAnalyzer obj(precision, constants_, functions_);
+    obj.analyze(strExpression, calcResult);
+
+    return calcResult;
 }
--- lib/string_math.h
+++ lib/string_math.h
@@ -5,86 +5,89 @@
  * \details
  * Class description
  * -----
+ * The class object performs calculations from a given mathematical expression
+ * in string format. The result of the calculation can be a 'double' value or a
+ * string with a given precision.
+ *
+ * The expression can contain subexpresions in brackets of diffrent types
+ * ('()', '[]' and '{}'). The object can compute functions (see 'functions_'
+ * list). Their names are taken from MATLAB. The object also supports constants
+ * such as 'pi' (see 'constants_' list).
+ *
+ * You can add, replace, or remove constants and functions via the class
+ * interface. For more information, see classes 'StringMathConstant' and
+ * 'StringMathFunction'.
+ *
+ * If the object finds a syntax error or, for example, a division by zero, it
+ * throws an exception of the 'StringMathError' type.
+ *
+ * Code example
+ * -----
+ * \code
+ * try
+ * {
+ *     QString exp_str = "6/2*(2+1)";
+ *
+ *     StringMath obj;
+ *     double result = obj.calculate(exp_str);
+ *     //The result will be 9.
+ * }
+ * catch(StringMathError &err)
+ * {
+ *     //Error handling (use err.what()).
+ * }
+ * \endcode
  */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 #ifndef STRING_MATH_H
 #define STRING_MATH_H
 
-#include    "expression_analyzer.h"
+#include "expression_analyzer.h"
 
 class STRINGMATH_EXPORT StringMath
 {
 public:
-    explicit StringMath(int precisionValue = 8);
-    //Functions...
-    void    set_precision(int precisionValue);
-    int     precision() const;
+    StringMath();
+    // Functions...
+    void add_constant(const StringMathConstant& newConstant);
+    void replace_constant(const QString& existingConstantName,
+                          double         newConstantValue);
+    void remove_constant(const StringMathConstant& existingConstant);
+    const QVector<StringMathConstant>& constants() const;
 
-    void    add_constant(const StringMathConstant &newConstant);
-    void    replace_constant(const QString &existingConstantName, double newConstantValue);
-    void    remove_constant(const StringMathConstant &existingConstant);
-    const QVector<StringMathConstant>   &constants() const;
+    void add_function(const StringMathFunction& newFunction);
+    void replace_function(const QString&                existingFunctionName,
+                          std::function<double(double)> newFunction);
+    void remove_function(const StringMathFunction& existingFunction);
+    const QVector<StringMathFunction>& functions() const;
 
-    void    add_function(const StringMathFunction &newFunction);
-    void    replace_function(const QString &existingFunctionName, std::function<double (double)> newFunction);
-    void    remove_function(const StringMathFunction &newFunction);
-    const QVector<StringMathFunction>   &functions() const;
+    double   calculate(const QString& strExpression) const;
+    QString& calculate(const QString& strExpression, int precision) const;
 
-    void    calculate(const QString &strExpression, QString &calcResult) const;
 private:
-    StringMath(const StringMath &obj);
-    StringMath &operator=(const StringMath &obj);
-    //Constants...
-    static const int    min_precision_value = 0;
-    static const int    max_precision_value = 16;
-    //Variables...
-    int precision_ = min_precision_value;
-    //Containers...
-    QVector<StringMathConstant> constants_ = {StringMathConstant("pi", 3.1415926535897932384626433832795),
-                                              StringMathConstant("e", 2.7182818284590452353602874713527)};
+    StringMath(const StringMath& obj);
+    StringMath& operator=(const StringMath& obj);
+    // Constants...
+    static const int min_precision = 0;
+    static const int max_precision = 16;
+    // Containers...
+    QVector<StringMathConstant> constants_ = {
+        StringMathConstant("pi", 3.1415926535897932384626433832795),
+        StringMathConstant("e", 2.7182818284590452353602874713527)};
 
-    QVector<StringMathFunction> functions_ = {StringMathFunction("cos", StringMathFuncImpl::cos_impl),
-                                              StringMathFunction("sin", StringMathFuncImpl::sin_impl),
-                                              StringMathFunction("tan", StringMathFuncImpl::tan_impl),
-                                              StringMathFunction("cot", StringMathFuncImpl::cot_impl),
-                                              StringMathFunction("acos", StringMathFuncImpl::acos_impl),
-                                              StringMathFunction("asin", StringMathFuncImpl::asin_impl),
-                                              StringMathFunction("atan", StringMathFuncImpl::atan_impl),
-                                              StringMathFunction("acot", StringMathFuncImpl::acot_impl),
-                                              StringMathFunction("log", StringMathFuncImpl::log_impl),
-                                              StringMathFunction("log10", StringMathFuncImpl::log10_impl),
-                                              StringMathFunction("log2", StringMathFuncImpl::log2_impl),
-                                              StringMathFunction("exp", StringMathFuncImpl::exp_impl),
-                                              StringMathFunction("sqrt", StringMathFuncImpl::sqrt_impl)};
+    QVector<StringMathFunction> functions_ = {
+        StringMathFunction("cos", StringMathFuncImpl::cos_impl),
+        StringMathFunction("sin", StringMathFuncImpl::sin_impl),
+        StringMathFunction("tan", StringMathFuncImpl::tan_impl),
+        StringMathFunction("cot", StringMathFuncImpl::cot_impl),
+        StringMathFunction("acos", StringMathFuncImpl::acos_impl),
+        StringMathFunction("asin", StringMathFuncImpl::asin_impl),
+        StringMathFunction("atan", StringMathFuncImpl::atan_impl),
+        StringMathFunction("acot", StringMathFuncImpl::acot_impl),
+        StringMathFunction("log", StringMathFuncImpl::log_impl),
+        StringMathFunction("log10", StringMathFuncImpl::log10_impl),
+        StringMathFunction("log2", StringMathFuncImpl::log2_impl),
+        StringMathFunction("exp", StringMathFuncImpl::exp_impl),
+        StringMathFunction("sqrt", StringMathFuncImpl::sqrt_impl)};
 };
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-/*!
- * The calculation precision getter function.
- * \return The precision of calculation result (an integer value in range from 'min_precision_value' to 'max_precision_value').
-*/
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-inline int StringMath::precision() const
-{
-    return precision_;
-}
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-/*!
- * The calculation constants getter function.
- * \return The container with constants (StringMathConstant objects).
-*/
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-inline const QVector<StringMathConstant> &StringMath::constants() const
-{
-    return constants_;
-}
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-/*!
- * The algebraic functions getter function.
- * \return The container with functions (StringMathFunction objects).
-*/
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-inline const QVector<StringMathFunction> &StringMath::functions() const
-{
-    return functions_;
-}
 #endif // STRING_MATH_H
