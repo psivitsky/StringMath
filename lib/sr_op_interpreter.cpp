7a209c5a6cf1d92ab70025e665f617dcf2d4ec09
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-03-05 10:12:27 +0300

    SROpInterpreter class updated
    SROpInterpreter class now uses ExpressionSymbol objects when processing expression.

--- lib/sr_op_interpreter.cpp
+++ lib/sr_op_interpreter.cpp
@@ -4,63 +4,95 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 SROpInterpreter::SROpInterpreter()
 {
-
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 /*!
  * Second rank operators interpreter function.
  * \param[in] symbols Expression symbols to interpret.
  * \return The calculation result.
-*/
+ */
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-double SROpInterpreter::interpret(const QVector<StringMathSymbol> &symbols) const
+double
+SROpInterpreter::interpret(const QVector<ExpressionSymbol*>& symbols) const
 {
-    QVector<StringMathSymbol>::const_iterator iterator = symbols.begin();
+    QVector<ExpressionSymbol*>::const_iterator iterator = symbols.begin();
 
-    StringMathSymbol first_operand = *iterator;
-    if(first_operand.type == emptyOperandType)
-    {
-        first_operand.type = operandType;
-        first_operand.value = 0.;
-    }
-
-    if(first_operand.type != operandType)
-        throw StringMathError("SROpInterpreter: the operand is invalid!");
-
-    double calc_result = first_operand.value;
-
+    ExpressionOperator* expression_operator = nullptr;
+    ExpressionOperand*  operand = operand_checker(iterator, symbols.end());
     ++iterator;
+
+    if(operand->is_empty())
+        operand->set_value(0.);
+
+    double calc_result = operand->value();
+
     while(iterator != symbols.end())
     {
-        if(iterator->type == sumOperatorType)
-        {
-            ++iterator;
-
-            if(iterator == symbols.end())
-                throw StringMathError("SROpInterpreter: an operand is missing!");
-
-            if(iterator->type != operandType)
-                throw StringMathError("SROpInterpreter: the operand is invalid!");
-
-            calc_result += iterator->value;
-        }
-        else if(iterator->type == diffOperatorType)
-        {
-            ++iterator;
-
-            if(iterator == symbols.end())
-                throw StringMathError("SROpInterpreter: an operand is missing!");
-
-            if(iterator->type != operandType)
-                throw StringMathError("SROpInterpreter: the operand is invalid!");
-
-            calc_result -= iterator->value;
-        }
-        else
-            throw StringMathError("SROpInterpreter: there is no such second rank operator!");
-
+        expression_operator = operator_checker(iterator, symbols.end());
         ++iterator;
+        operand = operand_checker(iterator, symbols.end());
+        ++iterator;
+
+        if(expression_operator->operator_type() == sumType)
+            calc_result += operand->value();
+        else if(expression_operator->operator_type() == diffType)
+            calc_result -= operand->value();
+        else
+            throw StringMathError(
+                "SROpInterpreter: there is no such second rank operator!");
     }
 
     return calc_result;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+/*!
+ * Operator checker function.
+ * \param[in] begin The iterator pointing to the operator in the parsing
+ * container.
+ * \param[in] end The iterator pointing to a symbol after the last
+ * symbol in the parsing container.
+ * \return centerOperator Pointer to the operator.
+ */
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+ExpressionOperator* SROpInterpreter::operator_checker(
+    QVector<ExpressionSymbol*>::const_iterator begin,
+    QVector<ExpressionSymbol*>::const_iterator end) const
+{
+    if(begin == end)
+        throw StringMathError("SROpInterpreter: the expression syntax error!");
+
+    ExpressionOperator* expression_operator = 0;
+
+    if((*begin)->symbol_type() == operatorType)
+        expression_operator = dynamic_cast<ExpressionOperator*>(*begin);
+    else
+        throw StringMathError("SROpInterpreter: the expression syntax error!");
+
+    return expression_operator;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+/*!
+ * Operand checker function.
+ * \param[in] begin The iterator pointing to the operand in the parsing
+ * container.
+ * \param[in] end The iterator pointing to a symbol after the last
+ * symbol in the parsing container.
+ * \return Pointer to the operand.
+ */
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+ExpressionOperand* SROpInterpreter::operand_checker(
+    QVector<ExpressionSymbol*>::const_iterator begin,
+    QVector<ExpressionSymbol*>::const_iterator end) const
+{
+    if(begin == end)
+        throw StringMathError("SROpInterpreter: the expression syntax error!");
+
+    ExpressionOperand* operand = 0;
+
+    if((*begin)->symbol_type() == operandType)
+        operand = dynamic_cast<ExpressionOperand*>(*begin);
+    else
+        throw StringMathError("SROpInterpreter: the expression syntax error!");
+
+    return operand;
 }
--- lib/sr_op_interpreter.h
+++ lib/sr_op_interpreter.h
@@ -10,18 +10,30 @@
 #ifndef SROPINTERPRETER_H
 #define SROPINTERPRETER_H
 
-#include    "string_math_base.h"
+#include "string_math_base.h"
 
-#include    <QVector>
+#include "expression.h"
+#include "expression_operand.h"
+#include "expression_operator.h"
+
+#include <QVector>
 
 class SROpInterpreter
 {
 public:
     SROpInterpreter();
-    //Functions...
-    double  interpret(const QVector<StringMathSymbol> &symbols) const;
+    // Functions...
+    double interpret(const QVector<ExpressionSymbol*>& symbols) const;
+
 private:
-    SROpInterpreter(const SROpInterpreter &obj);
-    SROpInterpreter &operator=(const SROpInterpreter &obj);
+    SROpInterpreter(const SROpInterpreter& obj);
+    SROpInterpreter& operator=(const SROpInterpreter& obj);
+    // Functions...
+    ExpressionOperator*
+    operator_checker(QVector<ExpressionSymbol*>::const_iterator begin,
+                     QVector<ExpressionSymbol*>::const_iterator end) const;
+    ExpressionOperand*
+    operand_checker(QVector<ExpressionSymbol*>::const_iterator begin,
+                    QVector<ExpressionSymbol*>::const_iterator end) const;
 };
 #endif // SROPINTERPRETER_H
