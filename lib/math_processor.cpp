83972b2abae72db7a4b2de45588d00f16688da99
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-03-16 17:38:33 +0300

    MathProcessor namespace updated

--- lib/math_processor.cpp
+++ lib/math_processor.cpp
@@ -1,35 +1,40 @@
 #include "math_processor.h"
-
+//----------------------------------------------------------------------------------
+/*!
+ * Expression calculation function.
+ * \param[in] expression The expression to calculate.
+ * \param[in] functions The container with functions.
+ * \return The result of the calculation.
+ */
+//----------------------------------------------------------------------------------
 double MathProcessor::expression_calculation(
     const Expression* expression, const QVector<StringMathFunction>& functions)
 {
-    QVector<ExpressionSymbol*> symbols_without_subexpressions;
+    QVector<ExpressionSymbol*> symbols_to_interpret;
+    QVector<ExpressionSymbol*> interpreted_symbols;
 
     foreach(ExpressionSymbol* symbol, expression->symbols())
     {
         if(symbol->symbol_type() == expressionType)
         {
-            Expression* subexpression = dynamic_cast<Expression*>(symbol);
-            ExpressionOperand* operand = new ExpressionOperand;
-            operand->set_value(
-                expression_calculation(subexpression, functions));
+            double subexpression_val = expression_calculation(
+                dynamic_cast<Expression*>(symbol), functions);
 
-            symbols_without_subexpressions.push_back(operand);
+            ExpressionOperand* operand = new ExpressionOperand();
+            operand->set_value(subexpression_val);
+
+            symbols_to_interpret.push_back(operand);
         }
         else
-            symbols_without_subexpressions.push_back(symbol);
+            symbols_to_interpret.push_back(symbol);
     }
 
-    FuncInterpreter function_(functions);
     FROpInterpreter first_rank_operators_;
+    first_rank_operators_.interpret(symbols_to_interpret, interpreted_symbols);
+
     SROpInterpreter second_rank_operators_;
-
-    QVector<ExpressionSymbol*> interpreted_symbols;
-    first_rank_operators_.interpret(symbols_without_subexpressions,
-                                    interpreted_symbols);
-
     double calc_result = second_rank_operators_.interpret(interpreted_symbols);
-    calc_result = function_.interpret(calc_result, expression->function_name());
 
-    return calc_result;
+    FuncInterpreter function_(functions);
+    return function_.interpret(calc_result, expression->function_name());
 }
