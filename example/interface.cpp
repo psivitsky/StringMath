28f3223b969f3826fa0d8970f0f0b1ca8c58827c
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-03-30 10:09:33 +0300

    The example application source code refactored

--- example/interface.cpp
+++ example/interface.cpp
@@ -1,36 +1,53 @@
 #include "interface.h"
 #include "ui_interface.h"
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+double sqr_example(double);
+
+//----------------------------------------------------------------------------------
 /*!
  * The constructor.
- * \param[in] parent Pointer to the widget parent.
-*/
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-Interface::Interface(QWidget *parent) : QWidget(parent), ui(new Ui::Interface)
+ * \param[in] parent Pointer to the parent widget.
+ */
+//----------------------------------------------------------------------------------
+Interface::Interface(QWidget* parent) : QWidget(parent), ui(new Ui::Interface)
 {
     ui->setupUi(this);
+    ui->expression_result_SB->setRange(-__DBL_MAX__, __DBL_MAX__);
 
-    ui->expression_result_SB->setRange( - __DBL_MAX__, __DBL_MAX__);
+    calc.add_constant(StringMathConstant("2pi", 2 * M_PI));
+    calc.add_function(StringMathFunction("sqr", sqr_example));
 }
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
 //! \details The destructor.
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
 Interface::~Interface()
 {
     delete ui;
 }
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
 //! \details The expression line edit slot.
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
 void Interface::on_expression_LE_editingFinished()
 {
+    ui->log_LE->clear();
+
     try
     {
-        StringMath calc(3);
-        ui->expression_result_SB->setValue(calc.string_process(ui->expression_LE->text()));
+        ui->expression_result_SB->setValue(
+            calc.calculate(ui->expression_LE->text()));
     }
-    catch(StringMathError &err)
+    catch(StringMathError& err)
     {
-        ui->log_TE->append(err.msg);
+        ui->log_LE->setText(err.what());
     }
+}
+//----------------------------------------------------------------------------------
+/*!
+ * Example of a square calculation function.
+ * \param[in] val Function argument.
+ */
+//----------------------------------------------------------------------------------
+double sqr_example(double val)
+{
+    return val * val;
 }
--- example/interface.h
+++ example/interface.h
@@ -1,32 +1,40 @@
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-/*!
- * \brief
- * The "StringMath" test widget application
- * \details
- * The widget contains a line edit for entering an expression and spin box for displaying the calculation result.\n
- * If a calculation throws an exception, it will be printed in log text edit.
- */
-//---------------------------------------------------------------------------------------------------------------------------------------------------
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#include    <QWidget>
+namespace Ui
+{
+class Interface;
+}
 
-#include    "string_math.h"
+//----------------------------------------------------------------------------------
+/*!
+ * \brief
+ * Example application of using the "StringMath" library
+ * \details
+ * The widget contains a line edit for entering an expression and spin box for
+ * displaying the calculation result.\n
+ * If a calculation throws an exception, it will be printed in log line edit.
+ */
+//----------------------------------------------------------------------------------
 
-namespace Ui { class Interface; }
+#include <QWidget>
+
+#include "string_math.h"
 
 class Interface : public QWidget
 {
     Q_OBJECT
 public:
-    explicit Interface(QWidget *parent = 0);
+    explicit Interface(QWidget* parent = 0);
+    Interface(const Interface& obj) = delete;
+    Interface& operator=(const Interface& obj) = delete;
     ~Interface();
 private slots:
-    void    on_expression_LE_editingFinished();
+    void on_expression_LE_editingFinished();
+
 private:
-    Ui::Interface *ui;
-    Interface(const Interface &obj);
-    Interface &operator=(const Interface &obj);
+    Ui::Interface* ui;
+    // Variables...
+    StringMath calc;
 };
 #endif // INTERFACE_H
