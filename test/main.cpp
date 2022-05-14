316f11c8d6c7b6f8d071c47ba6993b17978b8c68
Pavel Sivitsky <92302433+psivitsky@users.noreply.github.com>
2022-05-14 16:26:13 +0300

    Comment correction

--- test/main.cpp
+++ test/main.cpp
@@ -4,7 +4,7 @@
  * Console application for testing the "StringMath" library
  * \details
  * The application runs several simple random tests of basic mathematical
- * operations. Specific tests are taken from the text file "rest.txt" in
+ * operations. Specific tests are taken from the text file "test.txt" in
  * resources.qrc. The test syntax in the text file is as follows:
  * |test_name,calculation_precision,expression_string=calculation_result|
  */
